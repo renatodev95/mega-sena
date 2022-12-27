862a9887f58aad10ffe4451b99dd096660640900
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-12-27 10:46:57 -0300

    utilizando o ngModel para passar o parâmetro da quantidade de números a serem sorteados criando um service para fornecer os números aleatórios ao componente

--- mega-sena-frontend/src/app/gerador/gerador.module.ts
+++ mega-sena-frontend/src/app/gerador/gerador.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
--- mega-sena-frontend/src/app/gerador/gerador/gerador.component.ts
+++ mega-sena-frontend/src/app/gerador/gerador/gerador.component.ts
@@ -1,24 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { GeradorService } from '../services/gerador.service';
 
 @Component({
   selector: 'app-gerador',
   templateUrl: './gerador.component.html',
-  styleUrls: ['./gerador.component.scss']
+  styleUrls: ['./gerador.component.scss'],
 })
 export class GeradorComponent implements OnInit {
 
-  value = 'Clear me';
-  lista: Number[] = []
+  value = '';
+  lista: Number[] = [];
 
-  constructor() { }
+  constructor(private service: GeradorService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   sortear() {
-    this.lista = []
-    for (let i=0; i<6;i++) {
-      this.lista.push(Math.floor(Math.random() * 60) + 1);
-    }
+    this.lista = this.service.getRandomNumbers(this.value);
   }
 }
